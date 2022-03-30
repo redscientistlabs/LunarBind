66d34db2bae0865dea14940059d8bdb19564cc99
NullShock78 <kylestamant1@gmail.com>
2022-03-30 17:35:48 -0400

    Started improvements for Unity. Not stable

---
+++ Source/LunarBindUnity/bin/Plugins/LunarBind/QuickScripting.cs
@@ -0,0 +1,67 @@
+﻿namespace LunarBind
+{
+    using MoonSharp.Interpreter;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// This class is for quick testing of scripts using global bindings. If you do not need bindings, use <see cref="MoonSharp.Interpreter.Script.RunString(string)"/>
+    /// </summary>
+    public static class QuickScripting
+    {
+        public static BasicScriptRunner Basic { get; private set; } = new BasicScriptRunner();
+        public static HookedScriptRunner Hooked { get; private set; } = new HookedScriptRunner();
+
+        public static void Run(string script)
+        {
+            Basic = new BasicScriptRunner();
+            Basic.Run(script);
+        }
+
+        public static void Run(string script, ScriptBindings bindings)
+        {
+            Basic = new BasicScriptRunner();
+            Basic.AddBindings(bindings);
+            Basic.Run(script);
+        }
+
+        public static void Run(string script, object target)
+        {
+            var bindings = new ScriptBindings(target);
+            Basic = new BasicScriptRunner(bindings);
+            Basic.Run(script);
+        }
+
+        /// <summary>
+        /// Creates a MoonSharp script with bindings
+        /// </summary>
+        /// <param name="bindings"></param>
+        /// <returns></returns>
+        public static Script CreateBoundScript(ScriptBindings bindings = null, CoreModules modules = CoreModules.Preset_HardSandbox | CoreModules.Coroutine | CoreModules.OS_Time)
+        {
+            Script script = new Script(modules);
+            GlobalScriptBindings.Initialize(script);
+            bindings.Initialize(script);
+            return script;
+        }
+    }
+
+    public static class Optimizers
+    {
+        public static DynValue CreateOptimizedAutoCoroutine(Script script, string functionName, int numArgs = 0)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < numArgs; i++)
+            {
+                if(i == 0) { sb.Append("arg0"); }
+                else { sb.Append(",arg"); sb.Append(i); }
+            }
+            string guid = "_OriginalOptimizedFunction_"+ Guid.NewGuid().ToString().Replace('-', '_');
+            string paramStr = sb.ToString();
+            string overrideStr = $"{guid} = {functionName} function {functionName}({paramStr}){Environment.NewLine} while true do{Environment.NewLine}  {guid}({paramStr}){Environment.NewLine}  coroutine.yield(){Environment.NewLine}  end{Environment.NewLine}end return{functionName}";
+            return script.DoString(overrideStr);
+        }
+    }
+
+}
---
+++ Source/LunarBindUnity/bin/Plugins/LunarBind/UnityHelper.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LunarBind;
+using LunarBind.Yielding;
+using UnityEngine;
+
+namespace LunarBind
+{
+    [LunarBindIgnoreAssemblyAdd]
+    public static class UnityHelper
+    {
+        static GameObject coroutineRunnerGob = null;
+
+        static LBCoroutineRunner coroutineRunner = null;
+        public static LBCoroutineRunner CoroutineRunner
+        {
+            get
+            {
+                EnsureRunnerExists();
+                return coroutineRunner;
+            }
+        }
+
+        private static void EnsureRunnerExists()
+        {
+            if (coroutineRunnerGob == null)
+            {
+                coroutineRunnerGob = new GameObject("LB_Unity_Coroutine_Helper");
+                coroutineRunnerGob.transform.position = Vector3.zero;
+                coroutineRunnerGob.isStatic = true;
+                coroutineRunner = coroutineRunnerGob.AddComponent<LBCoroutineRunner>();
+            }
+        }
+        [UnityEngine.RuntimeInitializeOnLoadMethod(UnityEngine.RuntimeInitializeLoadType.AfterAssembliesLoaded)]
+        static void OnAfterAssembliesLoadRuntimeMethod()
+        {
+            GlobalScriptBindings.RegisterUserDataType(typeof(IEnumerator));
+            GlobalScriptBindings.BindTypeFuncs(typeof(UnityHelper));
+        }
+
+        //[LunarBindHide]
+        [LunarBindDocumentation("Starts an IEnumerator coroutine and continues without waiting for completion")]
+        [LunarBindFunction("Unity.StartCoroutine")]
+        static void StartUnityCoroutine(IEnumerator coroutine)
+        {
+            CoroutineRunner.RunCoroutine(coroutine);
+        }
+
+        //[LunarBindHide]
+        [LunarBindDocumentation("Starts an IEnumerator coroutine and waits for it to complete. Identical to calling coroutine.yield(YourFunc)")]
+        [LunarBindFunction("Unity.AwaitCoroutine")]
+        static WaitForDone WaitForUnityCoroutine(IEnumerator coroutine)
+        {
+            return CoroutineRunner.WaitForCoroutine(coroutine);
+        }
+
+        public class LBCoroutineRunner : UnityEngine.MonoBehaviour
+        {
+            public WaitForDone WaitForCoroutine(IEnumerator coroutine)
+            {
+                var yielder = new WaitForDone();
+                IEnumerator Routine(WaitForDone waitForDone)
+                {
+                    yield return coroutine;
+                    waitForDone.Done = true;
+                }
+                StartCoroutine(Routine(yielder));
+                return yielder;
+            }
+
+            public void RunCoroutine(IEnumerator coroutine)
+            {
+                StartCoroutine(coroutine);
+            }
+        }
+
+    }
+}
